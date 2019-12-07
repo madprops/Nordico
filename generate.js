ea0511d122283f268e0e59f69b8393b3691e10e9
madprops <easywest@protonmail.com>
2019-12-07 04:53:57 -0600

    Update

--- generate.js
+++ generate.js
@@ -23,11 +23,17 @@ function replace(obj, path)
     }
 }
 
-fetch("https://raw.githubusercontent.com/arcticicestudio/nord-visual-studio-code/develop/themes/nord.json")
+fetch("https://raw.githubusercontent.com/arcticicestudio/nord-visual-studio-code/develop/themes/nord-color-theme.json")
 
 .then(res =>
 {
-    return res.json()
+    return res.text()
+})
+
+.then(text =>
+{
+    let text2 = text.split("\n").filter(x => !x.trim().startsWith("/*")).join("\n")
+    return JSON.parse(text2)
 })
 
 .then(obj =>
@@ -51,31 +57,37 @@ fetch("https://raw.githubusercontent.com/arcticicestudio/nord-visual-studio-code
         n += 1
     }
 
-    obj.colors["activityBar.background"] = "#282d38",
-    obj.colors["activityBarBadge.background"] = "#a9c3ca",
-    obj.colors["activityBarBadge.foreground"] = "#000000",
+    obj.colors["activityBar.background"] = "#282d38"
+    obj.colors["activityBarBadge.background"] = "#a9c3ca"
+    obj.colors["activityBarBadge.foreground"] = "#000000"
 
-    obj.colors["badge.background"] = "#a9c3ca",
+    obj.colors["badge.background"] = "#a9c3ca"
     obj.colors["badge.foreground"] = "#000000"
 
-    obj.colors["editor.selectionBackground"] = "#55807183",
-    obj.colors["editor.selectionHighlightBackground"] = "#434c5e7a",
+    obj.colors["editor.selectionBackground"] = "#55807183"
+    obj.colors["editor.selectionHighlightBackground"] = "#434c5e7a"
     obj.colors["editor.hoverHighlightBackground"] = "#55807183"
 
-    obj.colors["scrollbarSlider.background"] = "#31384699",	
+    obj.colors["scrollbarSlider.background"] = "#31384699"	
 
-    obj.colors["list.activeSelectionBackground"] = "#333b4b",
-    obj.colors["list.activeSelectionForeground"] = "#d8dee9",
-    obj.colors["list.inactiveSelectionBackground"] = "#333b4b",
-    obj.colors["list.inactiveSelectionForeground"] = "#d8dee9",
-    obj.colors["list.hoverForeground"] = "#e6e6e6",
-    obj.colors["list.focusForeground"] = "#e6e6e6",
-    obj.colors["list.focusBackground"] = "#3e4657",
-    obj.colors["list.hoverBackground"] = "#2d3341",
-    obj.colors["list.inactiveFocusBackground"] = "#534c722a",
+    obj.colors["list.activeSelectionBackground"] = "#333b4b"
+    obj.colors["list.activeSelectionForeground"] = "#d8dee9"
+    obj.colors["list.inactiveSelectionBackground"] = "#333b4b"
+    obj.colors["list.inactiveSelectionForeground"] = "#d8dee9"
+    obj.colors["list.hoverForeground"] = "#e6e6e6"
+    obj.colors["list.focusForeground"] = "#e6e6e6"
+    obj.colors["list.focusBackground"] = "#3e4657"
+    obj.colors["list.hoverBackground"] = "#2d3341"
+    obj.colors["list.inactiveFocusBackground"] = "#534c722a"
     obj.colors["list.dropBackground"] = "#565ca352"
 
-    obj.colors["tab.hoverBackground"] = "#383c4e6b",
+    obj.colors["tab.hoverBackground"] = "#383c4e6b"
+
+    obj.colors["editorIndentGuide.activeBackground"] = "#434c5eb3"
+    obj.colors["editorLineNumber.foreground"] = "#616E88"
+    obj.colors["editorLineNumber.Activeforeground"] = "#616E88"
+    obj.colors["editorCursor.foreground"] = "#6b7793"
+    obj.colors["editorBracketMatch.border"] = "#6c7895"
 
     fs.writeFile(path.join("themes", "Nordico.json"), JSON.stringify(obj, null, 4), 'utf8', err =>
     {
